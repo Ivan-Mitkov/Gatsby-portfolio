adb0136cd78b72b16c326bbca8ab2e1b60599572
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-07-12 12:39:06 +0300

    about page as single type in strapi

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,9 +3,52 @@ import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Title from "../components/Title"
 import Image from "gatsby-image"
-// ...GatsbyImageSharpFluid
-const About = () => {
-  return <h2>about page</h2>
+export const query = graphql`
+  {
+    about: allStrapiAbout {
+      nodes {
+        stack {
+          id
+          title
+        }
+        title
+        info
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
+const About = ({
+  data: {
+    about: { nodes },
+  },
+}) => {
+  console.log(nodes)
+  const { title, info, image, stack } = nodes[0]
+  return (
+    <Layout>
+      <section className="about-page">
+        <div className="section-center about-center">
+          <Image fluid={image.childImageSharp.fluid} className="about-img" />
+          <article className="about-text">
+            <Title title={title} />
+            <p>{info}</p>
+            <div className="about-stack">
+              {stack.map(s => {
+                return <span key={s.id}>{s.title}</span>
+              })}
+            </div>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  )
 }
 
 export default About
